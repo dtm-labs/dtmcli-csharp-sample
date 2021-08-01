a64eb10ce61b79b0a912546011850c5d5cf10951
geffzhang <geffzhang@qq.com>
2021-08-01 14:24:43 +0800

    feat: update dtmcli package

--- DtmTccSample/DtmTccSample/Controllers/HomeController.cs
+++ DtmTccSample/DtmTccSample/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Dtmcli;
 using System.Threading;
+using Microsoft.Extensions.Logging;
 
 namespace DtmTccSample.Controllers
 {
@@ -13,10 +14,12 @@ namespace DtmTccSample.Controllers
     public class HomeController : ControllerBase
     {
         TccGlobalTransaction globalTransaction;
+        ILogger logger;
 
-        public HomeController(TccGlobalTransaction transaction)
+        public HomeController(TccGlobalTransaction transaction,ILoggerFactory loggerFactory)
         {
             globalTransaction = transaction;
+            logger = loggerFactory.CreateLogger<HomeController>();
         }
 
         [HttpPost]
@@ -29,9 +32,11 @@ namespace DtmTccSample.Controllers
             {
                 await globalTransaction.Excecute(async (tcc) =>
                 {
-                    await tcc.CallBranch(request, svc + "/TransOut/Try", svc + "/TransOut/Confirm", svc + "/TransOut/Cancel", cts.Token);
-                    await tcc.CallBranch(request, svc + "/TransIn/Try", svc + "/TransIn/Confirm", svc + "/TransIn/Cancel",cts.Token);
+                    var res1 = await tcc.CallBranch(request, svc + "/TransOut/Try", svc + "/TransOut/Confirm", svc + "/TransOut/Cancel", cts.Token);
+                    var res2 = await tcc.CallBranch(request, svc + "/TransIn/Try", svc + "/TransIn/Confirm", svc + "/TransIn/Cancel",cts.Token);
+                    logger.LogInformation($"tcc returns: {res1}-{res2}");
                 }, cts.Token);
+                
             }
             catch(Exception ex)
             {
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # dtmcli-csharp-sample
+dtmcli c# 使用示例
+
+## 快速开始
+### 部署启动dtm
+需要docker版本18以上
+```
+git clone https://github.com/yedf/dtm
+cd dtm
+docker-compose up
+```
+### 启动示例
+```
+cd DtmTccSample
+dotnet run DtmTccSample.csproj
+```
+### 运行示例
+
+使用浏览器访问 http://192.168.5.9:5000/swagger/index.html
+
+执行 ​/api​/Home​/Demo
+
+### 示例解读
+核心代码如下，示例开启一个tcc全局事务，然后在事务内部注册并调用TransOut和TransIn 分支，完成后返回，剩下的二阶段Comfirm，会由DTM完成
+```C# 
+
+var svc = "http://192.168.5.9:5000/api";
+TransRequest request = new TransRequest() { Amount = 30 };
+var cts = new CancellationTokenSource();
+// 开启一个tcc全局事务   
+await globalTransaction.Excecute(async (tcc) =>
+{
+     //调用TransOut分支，四个参数分别为post的body，tryUrl, confirmUrl, cancelUrl
+      await tcc.CallBranch(request, svc + "/TransOut/Try", svc + "/TransOut/Confirm", svc + "/TransOut/Cancel", cts.Token);
+      //调用TransIn分支
+      await tcc.CallBranch(request, svc + "/TransIn/Try", svc + "/TransIn/Confirm", svc + "/TransIn/Cancel",cts.Token);
+    }, cts.Token);
+}
+```
