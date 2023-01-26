c0b8b234b337960e1590e71024c8cdd1103bb319
shengjie <ysjshengjie@live.cn>
2023-01-26 10:15:45 +0800

    Integrate docker compose with project so that we can start and debug it in visual studio.

---
+++ DtmSample/docker/mongo/dtmcli.barrier.mongo.js
@@ -0,0 +1,2 @@
+db.barrier.createIndex({ gid: 1, branch_id: 1, op: 1, barrier_id: 1 }, { unique: true });
+db.barrier.insert({ gid: "123", branch_id: "01", op: "action", barrier_id: "01", reason: "test" });
